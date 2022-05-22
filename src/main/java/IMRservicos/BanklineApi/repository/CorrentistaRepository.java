8855b0fd21c639a1ee238cf7da747df7fe527379
Paulosraiva41 <paulosraiva@hotmail.com>
2022-05-21 22:40:07 -0300

    APIRestJava

---
+++ src/main/java/IMRservicos/BanklineApi/repository/CorrentistaRepository.java
@@ -0,0 +1,7 @@
+package IMRservicos.BanklineApi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CorrentistaRepository extends JpaRepository<Correntista, Integer>{
+
+}
